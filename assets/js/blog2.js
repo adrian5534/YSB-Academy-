eabba3cd809cb37716413cf5a379eb53739233a2
adrian5534 <81486606+adrian5534@users.noreply.github.com>
2023-06-22 08:17:09 -0500

    blog update

---
+++ assets/js/blog2.js
@@ -0,0 +1,26 @@
+// filter Js 
+$(document).ready(function(){
+    $('.filter-item').click(function(){
+        const vaule = $(this).attr('data-filter')
+        if (vaule == 'all') {
+            $('.post-box').show('1000')
+        } else {
+            $(".post-box")
+            .not("." + vaule)
+            .hide("1000");
+            $(".post-box")
+            .filter("." + vaule)
+            .show("1000");
+        }
+    });
+    // Add active to btn 
+    $(".filter-item").click(function(){
+        $(this).addClass("active-filter").siblings().removeClass("active-filter");
+    });
+});
+
+//let section = document.querySelector('section')
+
+//window.addEventListener('scroll', () => {
+  //  section.classList.toggle('shadow', window.scrollY > 0);
+//});
