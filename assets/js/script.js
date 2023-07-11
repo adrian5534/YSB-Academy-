d91c67c32b3e8b95705a690c082caac00f0fcbf5
adrian5534 <81486606+adrian5534@users.noreply.github.com>
2023-07-11 11:40:52 -0500

    contact, about, blog pages up date  update

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,17 +47,19 @@ addEventOnElem(navLinks, "click", closeNavbar);
  * header active when scroll down to 100px
  */
 
-const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector(".header");
+  const backTopBtn = document.querySelector(".back-top-btn");
 
-const activeElem = function () {
-  if (window.scrollY > 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-}
+  const activeElem = function () {
+    if (window.scrollY > 100) {
+      header.classList.add("active");
+      backTopBtn.classList.add("active");
+    } else {
+      header.classList.remove("active");
+      backTopBtn.classList.remove("active");
+    }
+  };
 
-addEventOnElem(window, "scroll", activeElem);
+  window.addEventListener("scroll", activeElem);
+});
