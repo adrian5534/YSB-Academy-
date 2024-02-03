ed76bcf17013b0756374f3cee58cf8a11a373846
adrian5534 <81486606+adrian5534@users.noreply.github.com>
2024-02-03 17:56:46 -0500

    update

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -17,6 +17,7 @@ function getLineItemsFromCart() {
 }
 
 async function createCheckoutSession(lineItems) {
+  console.log('lineItems:', lineItems); // Log lineItems
   const response = await fetch('https://9zxvvqfzs9.execute-api.us-east-2.amazonaws.com/dev/stripe/create-checkout-session', {
   method: 'POST',
   headers: {
